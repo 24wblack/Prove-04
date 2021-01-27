baae7ccf4f44d0c10d2c9181e8f4091a975959e6
Slyfluffy <Cur17018@byui.edu>
2021-01-26 19:38:40 -0700

    Added my Hello!

--- src/provewk04/Main.java
+++ src/provewk04/Main.java
@@ -3,6 +3,7 @@ package provewk04;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello from William Black.");
+        System.out.println("Hello from William Black.");
+        System.out.println("Hello William, my name is Journey.");
     }
 }
